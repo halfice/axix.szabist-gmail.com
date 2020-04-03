b811e6a027b9c7f496c6cd714442e82bdda4125b
axix.szabist@gmail.com <IA4188@adda.gov.ae>
2020-04-03 09:03:13 +0400

    final from dev

--- src/extensions/headerfooterextension/HeaderfooterextensionApplicationCustomizer.ts
+++ src/extensions/headerfooterextension/HeaderfooterextensionApplicationCustomizer.ts
@@ -12,7 +12,7 @@ import {
 
 
 import styles from './AppCustomizer.module.scss';
- 	import { escape } from '@microsoft/sp-lodash-subset';
+import { escape } from '@microsoft/sp-lodash-subset';
 
 
 import * as strings from 'HeaderfooterextensionApplicationCustomizerStrings';
@@ -39,9 +39,9 @@ export default class HeaderfooterextensionApplicationCustomizer
   extends BaseApplicationCustomizer<IHeaderfooterextensionApplicationCustomizerProperties> {
 
 
-    // These have been added
-    private _topPlaceholder: PlaceholderContent | undefined;
-    private _bottomPlaceholder: PlaceholderContent | undefined;
+  // These have been added
+  private _topPlaceholder: PlaceholderContent | undefined;
+  private _bottomPlaceholder: PlaceholderContent | undefined;
 
 
 
@@ -52,31 +52,31 @@ export default class HeaderfooterextensionApplicationCustomizer
 
 
       let articleRedirectScriptTag: HTMLScriptElement = document.createElement("script");
-      articleRedirectScriptTag.src = "https://azizi021.sharepoint.com/Style%20Library/myscript.js";
+      articleRedirectScriptTag.src = "https://abudhabidigital.sharepoint.com/Site%20Assets/adda.js";
       articleRedirectScriptTag.type = "text/javascript";
       document.body.appendChild(articleRedirectScriptTag);
 
-      
+
 
 
       const heads: any = document.getElementsByTagName("head")[0] || document.documentElement;
       let customJS: HTMLScriptElement = document.createElement("script");
       customJS.type = "text/javascript";
-      customJS.src = "https://azizi021.sharepoint.com/Style%20Library/myscript.js";
+      customJS.src = "https://abudhabidigital.sharepoint.com/Site%20Assets/adda.js";
       heads.insertAdjacentElement("beforeEnd", customJS);
 
 
 
-        // inject the style sheet
-        const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
-        let customStyle: HTMLLinkElement = document.createElement("link");
-        customStyle.href = cssUrl;
-        customStyle.rel = "stylesheet";
-        customStyle.type = "text/css";
-        head.insertAdjacentElement("beforeEnd", customStyle);
+      // inject the style sheet
+      const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
+      let customStyle: HTMLLinkElement = document.createElement("link");
+      customStyle.href = cssUrl;
+      customStyle.rel = "stylesheet";
+      customStyle.type = "text/css";
+      head.insertAdjacentElement("beforeEnd", customStyle);
     }
 
-    this.context.placeholderProvider.changedEvent.add(this,this._renderPlaceHolders);
+    this.context.placeholderProvider.changedEvent.add(this, this._renderPlaceHolders);
     return Promise.resolve();
   }
   private _renderPlaceHolders(): void {
@@ -84,21 +84,21 @@ export default class HeaderfooterextensionApplicationCustomizer
     console.log('Available placeholders: ',
       this.context.placeholderProvider.placeholderNames.map(name => PlaceholderName[name]).join(', '));
 
-      const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
-      let customStyle: HTMLLinkElement = document.createElement("link");
-      customStyle.href = "https://azizi021.sharepoint.com/SiteAssets/mine.css";
-      customStyle.rel = "stylesheet";
-      customStyle.type = "text/css";
-      head.insertAdjacentElement("beforeEnd", customStyle);
+    const head: any = document.getElementsByTagName("head")[0] || document.documentElement;
+    let customStyle: HTMLLinkElement = document.createElement("link");
+    customStyle.href = "https://abudhabidigital.sharepoint.com/Site%20Assets/adda.css";
+    customStyle.rel = "stylesheet";
+    customStyle.type = "text/css";
+    head.insertAdjacentElement("beforeEnd", customStyle);
 
 
-    if (!this._topPlaceholder){
-        this._topPlaceholder= this.context.placeholderProvider.tryCreateContent(
-      PlaceholderName.Top,{onDispose:this._onDispose}
-          );//try create method end
+    if (!this._topPlaceholder) {
+      this._topPlaceholder = this.context.placeholderProvider.tryCreateContent(
+        PlaceholderName.Top, { onDispose: this._onDispose }
+      );//try create method end
     }//top place holder
 
-    if (!this._topPlaceholder){
+    if (!this._topPlaceholder) {
       console.log("Place holder top not found");
       return;
     }
@@ -113,14 +113,19 @@ export default class HeaderfooterextensionApplicationCustomizer
         this._topPlaceholder.domElement.innerHTML = `
         <div class="${styles.app}">
           <div class="${styles.top}">
-           
+         
           </div>
-        </div>`;
+        </div>
+        <div class="${styles.logos}">
+        <a class="" href="/" tabindex="-1">
+        <img src="https://adda.gov.ae/-/media/Project/TAMM/ADDA/Logo/ADDA.svg" alt="Abu Dhabi Digital Authority"></a>
+        </div>
+        `;
       }
     }
 
     // Handling the bottom placeholder
- 		if (!this._bottomPlaceholder) {
+    if (!this._bottomPlaceholder) {
       this._bottomPlaceholder = this.context.placeholderProvider.tryCreateContent(
         PlaceholderName.Bottom,
         { onDispose: this._onDispose }
@@ -134,19 +139,17 @@ export default class HeaderfooterextensionApplicationCustomizer
 
       if (this.properties) {
         let bottomString: string = this.properties.Bottom;
-        if (!bottomString) {
-          bottomString = "© Copyright 2020 Half-Ice, LLC";
-        }
+
 
         if (this._bottomPlaceholder.domElement) {
           this._bottomPlaceholder.domElement.innerHTML = `
           <div class="${styles.app}">
             <div class="${styles.bottom}">
-              ${escape(
-                bottomString
-              )}
-  
-           
+            <div class="privacy-policy">
+            <div class="">
+            </div>
+            <div>© 2020 Abu Dhabi Government. All rights reserved</div>
+        </div>
           </div>`;
         }
       }
@@ -159,7 +162,7 @@ export default class HeaderfooterextensionApplicationCustomizer
 
   }
 
-  private _onDispose():void{
+  private _onDispose(): void {
     console.log("log has been dispose!!!!");
   }
 
